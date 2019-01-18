711e585f03542e37f9775f420b67930f7a44fbbb
Todd Trowbridge <toddtrowbridge@Brucebs-MacBook-Pro.local>
2019-01-18 16:03:26 -0700

    Initial commit with stubbed README.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Jets Application
+
+Description
+
+Technologies
+
+Image, maybe the UML I downloaded from Skill Distillery's GitHub page.
---
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.jets;
+
+public class JetsApplication {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
